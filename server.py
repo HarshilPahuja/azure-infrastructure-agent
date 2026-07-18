22b70490203781afa0aa21b6bb0337a65e433657
HarshilPahuja <harshilpahuja05@gmail.com>
2026-07-18 12:57:23 +0530

    done creating resource groups using agent

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from fastmcp import FastMCP
+from azure_client import resource_client
 
 mcp = FastMCP("Azure VM Agent")
 
@@ -7,3 +8,25 @@ mcp = FastMCP("Azure VM Agent")
 def hello(name: str) -> str:
     """Say hello to someone."""
     return f"Hello {name}! MCP is working!"
+
+@mcp.tool
+def create_resource_group(
+    name: str,
+    location: str
+):
+    """
+    Creates an Azure Resource Group.
+    """
+
+    rg = resource_client.resource_groups.create_or_update(
+        name,
+        {
+            "location": location
+        }
+    )
+
+    return {
+        "status": "success",
+        "resource_group": rg.name,
+        "location": rg.location
+    }
